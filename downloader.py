af45e0c15993d01711257a93ea40d256eb0c2b04
Kairav Parekh <9643321zapdos#gmail.com>
2020-11-18 16:21:28 +0530

    Updated downloader.py and added images folder

--- downloader.py
+++ downloader.py
@@ -6,7 +6,20 @@ def download(url, path):
 def main():
     url = input("Enter the youtube video URL: ")
     path = input("Enter the path where you want to save your video: ")
-    download(url, path)   
+
+    try:
+        download(url, path)
+    except KeyboardInterrupt:
+        print("Terminating Program...")
+        exit()
+    except:
+        print ("Something went worng. Please try again.")
+        print ("The possible issues are: ")
+        print ("1. The path provided is invalid.")
+        print ("2. The URL provided is invalid.")
+        exit()
+
+    print("Video downloaded to", path) 
 
 if __name__ == '__main__':
-    main()  
+    main()
